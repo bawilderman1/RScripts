fe6922a60c5abf3f005dd8cf00895a2dd54e2b1d
bawilderman1 <30907764+bawilderman1@users.noreply.github.com>
2025-08-25 00:13:53 -0500

    Saving checkpoint after successful dll compilation and before removing code systematically to isolate a bug in the backtest

---
+++ BacktestScriptsV2/test.R
@@ -0,0 +1,4 @@
+library(Rcpp)
+Rcpp::sourceCpp("C:/Users/bawil/Documents/RScripts/BacktestScriptsV2/test.cpp")
+result <- test_function()
+print(result)
---
+++ BacktestScriptsV2/test.cpp
@@ -0,0 +1,6 @@
+#include <Rcpp.h>
+
+// [[Rcpp::export]]
+int test_function() {
+  return 42;
+}
